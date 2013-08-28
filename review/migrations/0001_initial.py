d36ea87b455f68f9214e90df3ee4b8578b9bd9c6
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2013-08-28 08:37:00 +0200

    added migrations

---
+++ review/migrations/0001_initial.py
@@ -0,0 +1,160 @@
+# flake8: noqa
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Review'
+        db.create_table(u'review_review', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('content', self.gf('django.db.models.fields.TextField')(max_length=1024, blank=True)),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=5, blank=True)),
+            ('creation_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'review', ['Review'])
+
+        # Adding model 'ReviewExtraInfo'
+        db.create_table(u'review_reviewextrainfo', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('review', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['review.Review'])),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+        ))
+        db.send_create_signal(u'review', ['ReviewExtraInfo'])
+
+        # Adding model 'VotingCategory'
+        db.create_table(u'review_votingcategory', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal(u'review', ['VotingCategory'])
+
+        # Adding model 'VotingCategoryTranslation'
+        db.create_table(u'review_votingcategorytranslation', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['review.VotingCategory'])),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=2)),
+        ))
+        db.send_create_signal(u'review', ['VotingCategoryTranslation'])
+
+        # Adding model 'Voting'
+        db.create_table(u'review_voting', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('vote', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('review', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['review.Review'])),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['review.VotingCategory'])),
+        ))
+        db.send_create_signal(u'review', ['Voting'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Review'
+        db.delete_table(u'review_review')
+
+        # Deleting model 'ReviewExtraInfo'
+        db.delete_table(u'review_reviewextrainfo')
+
+        # Deleting model 'VotingCategory'
+        db.delete_table(u'review_votingcategory')
+
+        # Deleting model 'VotingCategoryTranslation'
+        db.delete_table(u'review_votingcategorytranslation')
+
+        # Deleting model 'Voting'
+        db.delete_table(u'review_voting')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'review.review': {
+            'Meta': {'ordering': "['-creation_date']", 'object_name': 'Review'},
+            'content': ('django.db.models.fields.TextField', [], {'max_length': '1024', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '5', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        u'review.reviewextrainfo': {
+            'Meta': {'ordering': "['type']", 'object_name': 'ReviewExtraInfo'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'review': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['review.Review']"}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'review.voting': {
+            'Meta': {'ordering': "['category', 'review']", 'object_name': 'Voting'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['review.VotingCategory']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'review': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['review.Review']"}),
+            'vote': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'review.votingcategory': {
+            'Meta': {'object_name': 'VotingCategory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'review.votingcategorytranslation': {
+            'Meta': {'object_name': 'VotingCategoryTranslation'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['review.VotingCategory']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'user_media.usermediaimage': {
+            'Meta': {'object_name': 'UserMediaImage'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'position': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['review']
