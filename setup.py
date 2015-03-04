329474fef5732c27720fded890616e22d62d3ff7
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-03-04 16:02:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
         'django',
         'django-user-media>=1.0',
         'django-generic-positions',
-        'django-hvad>=0.4.1',
+        'django-hvad>=0.4.1, <=1.0.0',
+        docutils >= 0.3, <=0.5
         'easy-thumbnails',
         'simplejson',
     ],
